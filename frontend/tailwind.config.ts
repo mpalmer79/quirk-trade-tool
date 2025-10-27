373dd6ab2fe8656cc8aff3f80e7a2556853b8acc
mpalmer79 <mpalm2025@gmail.com>
2025-10-27 16:24:18 -0400

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,12 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./app/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
-} satisfies Config;
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
