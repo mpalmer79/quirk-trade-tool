fe877cc53d2e9184ed272f9ad21771960cd05def
mpalmer79 <mpalm2025@gmail.com>
2025-11-03 16:22:12 -0500

    Update black-book-provider.ts

--- orchestrator/src/providers/black-book-provider.ts
+++ orchestrator/src/providers/black-book-provider.ts
@@ -1,1 +1,12 @@
+import { BaseProvider } from './base-provider';
+import type { ProviderConfig } from '../types/valuation.types';
 
+export class BlackBookProvider extends BaseProvider {
+  protected config: ProviderConfig = {
+    name: 'Black Book',
+    basePrice: 18000,
+    yearAdjustmentRate: 0.08,
+    mileageAdjustmentRate: 0.35,
+    randomVariance: 500
+  };
+}
