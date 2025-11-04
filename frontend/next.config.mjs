3c7a985193f0bea1cd130e48b6df03760b9634c9
mpalmer79 <mpalm2025@gmail.com>
2025-11-04 08:34:47 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-const isPages = process.env.GITHUB_PAGES === 'true';
-const repo = 'quirk-trade-tool';
-
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  ...(isPages && {
-    output: 'export',
-    trailingSlash: true,
-    images: { unoptimized: true },
-    basePath: `/${repo}`,
-    assetPrefix: `/${repo}/`,
-  }),
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/quirk-trade-tool' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
